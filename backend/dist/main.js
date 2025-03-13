f114395bfbbea426c87a8bc7e1b93a0fda0f2a0f
FailBird0 <desdonoc@gmail.com>
2025-03-13 15:27:52 +0100

    nightly. https :(

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -2,14 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const fs = require("fs");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const httpsOptions = {
+        key: fs.readFileSync(process.env.SSL_KEY),
+        cert: fs.readFileSync(process.env.SSL_CERT),
+        passphrase: process.env.SSL_PASS
+    };
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        httpsOptions
+    });
     app.enableCors({
-        allowedHeaders: '*',
-        origin: '*',
+        origin: ["http://localhost:5173", "https://localhost:5173", /192\.168\.1\.\d+/],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
     });
-    await app.listen(3000);
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.SSL_KEY),
+    cert: fs.readFileSync(process.env.SSL_CERT),
+    passphrase: process.env.SSL_PASS
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
 
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
+    origin: ["http://localhost:5173", "https://localhost:5173", /192\.168\.1\.\d+/],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
   });
   
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
